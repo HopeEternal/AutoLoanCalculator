b0c192486038d69b947a58f03bf1ae79ce59b7cf
HopeEternal <risalee316@gmail.com>
2019-09-24 21:04:42 -0600

    Fixed stacking Amortization Table

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import './styles/main.scss';
 
 //Event Listeners
-document.getElementById('inputForm').addEventListener('input', function(){setVariables()});
+document.getElementById('inputForm').addEventListener('input', function(){updateOutput()});
 document.getElementById('btnCalculate').addEventListener('click', function(){generateSchedule()});
 
 //Variables
@@ -10,58 +10,44 @@ const form = document.getElementById("inputForm");
 const monthlyOutput = document.getElementById("monthlyOutput");
 const interestOutput = document.getElementById("interestOutput");
 
-function setVariables() {
+function updateOutput() {
     let formData = {
-        carPrice: parseInt(form.elements["carPrice"].value),
-        downPayment: parseInt(form.elements["downPayment"].value),
-        loanYears: parseInt(form.elements["years"].value),
-        loanMonths: parseInt(form.elements["months"].value),
-        interestRate: parseInt(form.elements["interestRate"].value),
-        monthlyPayment: 0,
-        loanDuration: 0,
-        intRatePerMonth: 0,
+        carPrice: parseFloat(form.elements["carPrice"].value),
+        downPayment: parseFloat(form.elements["downPayment"].value),
+        loanYears: parseFloat(form.elements["years"].value),
+        loanMonths: parseFloat(form.elements["months"].value),
+        interestRate: parseFloat(form.elements["interestRate"].value),
+        calcMonthlyPayment: function () {
+            return ((this.intRatePerMonth() + (this.intRatePerMonth() / (Math.pow((1 + this.intRatePerMonth()), this.calcLoanDuration()) - 1))) * (this.carPrice - (this.downPayment || 0))).toFixed(2);
+        },
+        calcLoanDuration: function () {
+            return parseFloat((this.loanYears * 12) + this.loanMonths);
+        },
+        intRatePerMonth: function () {
+            return parseFloat(formData.interestRate / 1200);
+        },
         totalinterestPaid: 0
     }
-    updateOutput(formData);
+
+    formData.monthlyPayment = ((formData.intRatePerMonth + (formData.intRatePerMonth / (Math.pow((1 + formData.intRatePerMonth), formData.calcLoanDuration()) - 1))) * (formData.carPrice - (formData.downPayment || 0))).toFixed(2);
+
+    formData.totalinterestPaid = (parseFloat((formData.interestRate / 1200)) * parseFloat(formData.carPrice - formData.downPayment)).toFixed(2);
+
+    //Output to UI
+    monthlyOutput.value = formData.calcMonthlyPayment();
+    interestOutput.value = formData.totalinterestPaid;
+
     return formData;
 }
 
-//Form 
-function updateOutput(formData) {
-
-    //Calculate
-    /* Monthly Payment Formula:
-        monthlyPayment = carPrice - downPayment * 
-        (interest per month [(1 + interest per month) * months] /
-        (1 + interest per month) ^ months - 1
-    */
-
-    //Convert years to months to perform all future calculations based on months
-    formData.loanDuration = (formData.loanYears * 12) + formData.loanMonths;     
-    
-    //Calculate Monthly Payment
-    formData.intRatePerMonth = (formData.interestRate / 1200);
-    formData.monthlyPayment = ((formData.intRatePerMonth + (formData.intRatePerMonth / (Math.pow((1 + formData.intRatePerMonth), formData.loanDuration) -1))) * (formData.carPrice - (formData.downPayment || 0))).toFixed(2);
-    
-    //Calculate Total Interest Paid
-    /* Total Interest Paid Formula
-        interest = (interest rate/number of payments) * loan principal
-    */
-    formData.totalinterestPaid = (formData.interestRate / 1200) * (formData.carPrice - formData.downPayment);
-
-   //Output to UI
-    monthlyOutput.value = formData.monthlyPayment;
-    interestOutput.value = formData.totalinterestPaid;
-}
-
-
 // Amortization schedule
 function generateSchedule() {
-    let formData = setVariables();
+    let formData = updateOutput();
     if (formData.monthlyPayment === "0.00") {
         alert('Please complete the form first!');
 
     } else {
+        
         //Generate Table
         let tableLocation = document.getElementsByClassName("amortization")[0];
 
@@ -80,15 +66,29 @@ function generateSchedule() {
         }
         table.appendChild(columnTitle);
 
-        // Calculations
+        // Calculations for amortization table
+        let balance = parseFloat(formData.carPrice - formData.downPayment);
+        let totalInterestPaid = parseFloat(formData.totalinterestPaid);
+        let interest = parseFloat(formData.totalinterestPaid);
+        let rowContent = [];
+
+        for ( let i = 0; i < formData.calcLoanDuration(); i++ ) {
+
+            balance = balance - formData.calcMonthlyPayment() + interest;
+            
+            rowContent[i] = [ i+1, formData.calcMonthlyPayment(), (formData.calcMonthlyPayment() - interest).toFixed(2), (interest).toFixed(2), totalInterestPaid.toFixed(2), balance.toFixed(2) ];
+
+            interest = parseFloat(formData.intRatePerMonth() * balance);
+            totalInterestPaid += interest;
+        }
 
         // Generate table body
-        for ( let i = 0; i < formData.loanDuration; i++ ) {
+        for ( let i = 0; i < formData.calcLoanDuration(); i++ ) {
             let row = document.createElement("tr");
-            
+
             for ( let j = 0; j < 6; j++ ) {
                 let cell = document.createElement("td");
-                let cellContent = document.createTextNode(formData.monthlyPayment);
+                let cellContent = document.createTextNode(rowContent[i][j]);
                 cell.appendChild(cellContent);
                 row.appendChild(cell);
             }
@@ -97,6 +97,7 @@ function generateSchedule() {
         }
 
         table.appendChild(tableBody);
+        tableLocation.innerHTML = "";
         tableLocation.appendChild(table);
         table.setAttribute("border", "2");
     }
